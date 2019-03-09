9d438b1da56f98ea8914c2f344e6cd4df1bf1f08
joe <joeldrt@gmail.com>
2019-03-09 02:22:48 -0600

    primear etapa maquetación inmuebles-admin page

--- src/app/admin/estate/estate.component.ts
+++ src/app/admin/estate/estate.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstateComponent implements OnInit {
 
-  constructor() { }
+  filters = [
+    new PropertyFilter('nombre', 'nombre'),
+    new PropertyFilter('m2 terreno', 'm2t.'),
+    new PropertyFilter('m2 construcción', 'm2c.'),
+    new PropertyFilter('niveles', 'niv.'),
+  ]
+  filter: PropertyFilter;
+
+  constructor() { 
+    this.filter = new PropertyFilter('', '');
+  }
 
   ngOnInit() {
   }
 
+  changeFilter(new_filter: PropertyFilter) {
+    this.filter = new_filter;
+  }
+
+}
+
+class PropertyFilter {
+  constructor(
+    public nombre: string,
+    public short_name: string,
+  ) {}
 }
