f396ec156226131fc312bd4f6421c6ab5cc49977
joe <joeldrt@gmail.com>
2019-03-11 17:01:29 -0600

    mejoras formulario login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -11,6 +11,9 @@ import { ToastrService } from '../../services/toastr/toastr.service';
 })
 export class LoginComponent implements OnInit {
 
+  loading = false;
+  mensaje_loading = "";
+
   public email: string;
   public password: string;
 
@@ -24,12 +27,21 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    if (!this.email || !this.password) {
+      this.toastr.error('Nombre de usuario o contraseña inválidos', "Error");
+      return;
+    }
+    this.loading = true;
+    this.mensaje_loading = "Ingresando..."
     this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password).then(
       (credentials) => {
         this.router.navigate(['/admin']);
+        this.loading = false;
       },
       (error) => {
+        this.loading = false;
         this.toastr.error(error.message, "Error");
+        this.loading = false;
       }
     );
   }
